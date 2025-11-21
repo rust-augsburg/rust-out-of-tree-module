729c2bfff6d4831129023b921c834ceb003234cb
Aaron Erhardt <aaron.erhardt@t-online.de>
2025-11-21 19:15:08 +0100

    Add hello world

--- rust_out_of_tree.rs
+++ rust_out_of_tree.rs
@@ -1,37 +1,90 @@
 // SPDX-License-Identifier: GPL-2.0
 
-//! Rust out-of-tree sample
+// Copyright (C) 2024 Google LLC.
 
-use kernel::prelude::*;
+//! Rust misc device sample.
+
+use core::pin::Pin;
+
+use kernel::{
+    c_str,
+    device::Device,
+    fs::{File, Kiocb},
+    iov::IovIterDest,
+    miscdevice::{MiscDevice, MiscDeviceOptions, MiscDeviceRegistration},
+    prelude::*,
+    str::CString,
+    sync::aref::ARef,
+};
 
 module! {
-    type: RustOutOfTree,
-    name: "rust_out_of_tree",
-    authors: ["Rust for Linux Contributors"],
-    description: "Rust out-of-tree sample",
+    type: RustMiscDeviceModule,
+    name: "rust_misc_device",
+    authors: ["Lee Jones", "Aaron Erhardt"],
+    description: "Rust misc device sample",
     license: "GPL",
 }
 
-struct RustOutOfTree {
-    numbers: KVec<i32>,
+#[pin_data]
+struct RustMiscDeviceModule {
+    #[pin]
+    _miscdev: MiscDeviceRegistration<RustMiscDevice>,
 }
 
-impl kernel::Module for RustOutOfTree {
-    fn init(_module: &'static ThisModule) -> Result<Self> {
-        pr_info!("Rust out-of-tree sample (init)\n");
+impl kernel::InPlaceModule for RustMiscDeviceModule {
+    fn init(_module: &'static ThisModule) -> impl PinInit<Self, Error> {
+        pr_info!("Initialising Rust Misc Device Sample\n");
 
-        let mut numbers = KVec::new();
-        numbers.push(72, GFP_KERNEL)?;
-        numbers.push(108, GFP_KERNEL)?;
-        numbers.push(200, GFP_KERNEL)?;
+        let options = MiscDeviceOptions {
+            name: c_str!("rust-misc-device"),
+            mode: 0o766,
+        };
 
-        Ok(RustOutOfTree { numbers })
+        try_pin_init!(Self {
+            _miscdev <- MiscDeviceRegistration::register(options),
+        })
     }
 }
 
-impl Drop for RustOutOfTree {
-    fn drop(&mut self) {
-        pr_info!("My numbers are {:?}\n", self.numbers);
-        pr_info!("Rust out-of-tree sample (exit)\n");
+#[pin_data(PinnedDrop)]
+struct RustMiscDevice {
+    dev: ARef<Device>,
+}
+
+#[vtable]
+impl MiscDevice for RustMiscDevice {
+    type Ptr = Pin<KBox<Self>>;
+
+    fn open(_file: &File, misc: &MiscDeviceRegistration<Self>) -> Result<Pin<KBox<Self>>> {
+        let dev = ARef::from(misc.device());
+
+        dev_info!(dev, "Opening Rust Misc Device Sample\n");
+
+        KBox::try_pin_init(
+            try_pin_init! {
+                RustMiscDevice {
+                    dev: dev,
+                }
+            },
+            GFP_KERNEL,
+        )
+    }
+
+    fn read_iter(mut kiocb: Kiocb<'_, Self::Ptr>, iov: &mut IovIterDest<'_>) -> Result<usize> {
+        let me = kiocb.file();
+        dev_info!(me.dev, "Reading from Rust Misc Device Sample\n");
+
+        let num = 42;
+        let info = CString::try_from_fmt(fmt!("Hello world! My num is {num}."))?;
+        let read = iov.simple_read_from_buffer(kiocb.ki_pos_mut(), info.to_bytes())?;
+
+        Ok(read)
+    }
+}
+
+#[pinned_drop]
+impl PinnedDrop for RustMiscDevice {
+    fn drop(self: Pin<&mut Self>) {
+        dev_info!(self.dev, "Exiting the Rust Misc Device Sample\n");
     }
 }
